ef16bdf47dbd8ed818e4f84d7cae4dd99a59400c
myorb <itdep24@gmail.com>
2025-03-06 03:53:16 +0100

    .

--- apps/web/components/Header.tsx
+++ apps/web/components/Header.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
-import { Home } from 'lucide-react'
+import { Home, Github, LogIn } from 'lucide-react'
 
 export default function Header() {
     return (
         <div className="flex gap-2 items-center justify-between shadow-md">
             <Link href='/'><h1 className="text-3xl font-bold m-3 text-center flex items-center justify-center"><Home className="mr-2" />JSMD Experiences</h1></Link>
-            <Link href='/login' className='text-center'><p className="text-md font-bold m-3 text-center">Login</p></Link>
+            <div className='flex gap-2'>
+                <Link href='https://github.com/myorb/jsmd-test' target='_blank' className='text-center'><p className="text-md font-bold m-3 text-center flex"><Github /> Code</p></Link>
+                <Link href='/login' className='text-center'><p className="text-md font-bold m-3 text-center flex">Login<LogIn /></p></Link>
+            </div>
         </div>
     )
 }
